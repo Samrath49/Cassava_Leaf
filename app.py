faef0ce0dfb6cc0413f64e98aaceb85759f455f1
Samrath Chauhan <55148801+Samrath49@users.noreply.github.com>
2021-04-20 19:41:19 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,39 +1,27 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Jun 13 02:20:31 2020
-
-@author: Krish Naik
-"""
-
-# -*- coding: utf-8 -*-
-"""
-Created on Fri May 15 12:50:04 2020
-
-@author: krish.naik
-"""
-
-
 import numpy as np
 import pickle
 import pandas as pd
 #from flasgger import Swagger
-import streamlit as st 
+import streamlit as st
 
 from PIL import Image
 
-#app=Flask(__name__)
-#Swagger(app)
+# app=Flask(__name__)
+# Swagger(app)
 
-pickle_in = open("classifier.pkl","rb")
-classifier=pickle.load(pickle_in)
+pickle_in = open("classifier.pkl", "rb")
+classifier = pickle.load(pickle_in)
 
-#@app.route('/')
+# @app.route('/')
+
+
 def welcome():
     return "Welcome All"
 
-#@app.route('/predict',methods=["Get"])
-def predict_note_authentication(variance,skewness,curtosis,entropy):
-    
+# @app.route('/predict',methods=["Get"])
+
+
+def predict_note_authentication(variance, skewness, curtosis, entropy):
     """Let's Authenticate the Banks Note 
     This is using docstrings for specifications.
     ---
@@ -57,37 +45,50 @@ def predict_note_authentication(variance,skewness,curtosis,entropy):
     responses:
         200:
             description: The output values
-        
+
     """
-   
-    prediction=classifier.predict([[variance,skewness,curtosis,entropy]])
+
+    prediction = classifier.predict([[variance, skewness, curtosis, entropy]])
     print(prediction)
     return prediction
 
 
-
 def main():
-    st.title("Bank Authenticator")
+    st.title("PathoAI")
     html_temp = """
     <div style="background-color:tomato;padding:10px">
-    <h2 style="color:white;text-align:center;">Streamlit Bank Authenticator ML App </h2>
+    <h2 style="color:white;text-align:center;">Cassava Leaf Disease Detection ML App</h2>
     </div>
     """
-    st.markdown(html_temp,unsafe_allow_html=True)
-    variance = st.text_input("Variance","Type Here")
-    skewness = st.text_input("skewness","Type Here")
-    curtosis = st.text_input("curtosis","Type Here")
-    entropy = st.text_input("entropy","Type Here")
-    result=""
+
+    instructions = """
+    Either upload your own image or select from the sidebar to get a preconfigured image. 
+    The image you select or upload will be fed through the Deep Neural Network in real-time 
+    and the output will be displayed to the screen.
+        """
+    st.write(instructions)
+
+    file = st.file_uploader('Upload An Image')
+    dtype_file_structure_mapping = {
+        'All Images': 'consolidated', 'Images Used To Train The Model': 'train',
+        'Images Used To Tune The Model': 'valid', 'Images The Model Has Never Seen': 'test'
+    }
+    st.title("Here is the image you've selected")
+
+    st.markdown(html_temp, unsafe_allow_html=True)
+    variance = st.text_input("Variance", "Type Here")
+    skewness = st.text_input("skewness", "Type Here")
+    curtosis = st.text_input("curtosis", "Type Here")
+    entropy = st.text_input("entropy", "Type Here")
+    result = ""
     if st.button("Predict"):
-        result=predict_note_authentication(variance,skewness,curtosis,entropy)
+        result = predict_note_authentication(
+            variance, skewness, curtosis, entropy)
     st.success('The output is {}'.format(result))
     if st.button("About"):
         st.text("Lets LEarn")
         st.text("Built with Streamlit")
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     main()
-    
-    
-    
