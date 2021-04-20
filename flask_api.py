ede4239741ff9fa2da5edad0d323a41f9002a740
Samrath Chauhan <55148801+Samrath49@users.noreply.github.com>
2021-04-20 19:41:43 +0530

    Add files via upload

--- flask_api.py
+++ flask_api.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri May 15 12:50:04 2020
-
-@author: krish.naik
-"""
-
 from flask import Flask, request
 import numpy as np
 import pickle
@@ -12,19 +5,20 @@ import pandas as pd
 import flasgger
 from flasgger import Swagger
 
-app=Flask(__name__)
+app = Flask(__name__)
 Swagger(app)
 
-pickle_in = open("classifier.pkl","rb")
-classifier=pickle.load(pickle_in)
+pickle_in = open("classifier.pkl", "rb")
+classifier = pickle.load(pickle_in)
+
 
 @app.route('/')
 def welcome():
     return "Welcome All"
 
-@app.route('/predict',methods=["Get"])
+
+@app.route('/predict', methods=["Get"])
 def predict_note_authentication():
-    
     """Let's Authenticate the Banks Note 
     This is using docstrings for specifications.
     ---
@@ -48,17 +42,18 @@ def predict_note_authentication():
     responses:
         200:
             description: The output values
-        
+
     """
-    variance=request.args.get("variance")
-    skewness=request.args.get("skewness")
-    curtosis=request.args.get("curtosis")
-    entropy=request.args.get("entropy")
-    prediction=classifier.predict([[variance,skewness,curtosis,entropy]])
+    variance = request.args.get("variance")
+    skewness = request.args.get("skewness")
+    curtosis = request.args.get("curtosis")
+    entropy = request.args.get("entropy")
+    prediction = classifier.predict([[variance, skewness, curtosis, entropy]])
     print(prediction)
     return "Hello The answer is"+str(prediction)
 
-@app.route('/predict_file',methods=["POST"])
+
+@app.route('/predict_file', methods=["POST"])
 def predict_note_file():
     """Let's Authenticate the Banks Note 
     This is using docstrings for specifications.
@@ -68,19 +63,18 @@ def predict_note_file():
         in: formData
         type: file
         required: true
-      
+
     responses:
         200:
             description: The output values
-        
+
     """
-    df_test=pd.read_csv(request.files.get("file"))
+    df_test = pd.read_csv(request.files.get("file"))
     print(df_test.head())
-    prediction=classifier.predict(df_test)
-    
+    prediction = classifier.predict(df_test)
+
     return str(list(prediction))
 
-if __name__=='__main__':
-    app.run(host='0.0.0.0',port=8000)
-    
-    
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000)
